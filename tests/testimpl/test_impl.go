575cb36c4232427587d47239e58b837f386cc089
Ben Vaughan <160539772+ben-vaughan-nttd@users.noreply.github.com>
2026-08-31 15:21:55 -0500

    fix: consolidate Dependabot updates and Terratest 1.x (#25)

--- tests/testimpl/test_impl.go
+++ tests/testimpl/test_impl.go
@@ -15,7 +15,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMonitorActionGroupModule(t *testing.T, ctx types.TestContext) {
+func TestComposableMonitorActionGroupModule(t *testing.T, ctx types.TestContext) {
 	subscriptionId := os.Getenv("ARM_SUBSCRIPTION_ID")
 	if len(subscriptionId) == 0 {
 		t.Fatal("ARM_SUBSCRIPTION_ID environment variable is not set")
@@ -38,8 +38,8 @@ func TestMonitorActionGroupModule(t *testing.T, ctx types.TestContext) {
 	}
 
 	t.Run("doesMonitorActionGroupExist", func(t *testing.T) {
-		resourceGroupName := terraform.Output(t, ctx.TerratestTerraformOptions(), "resource_group_name")
-		MonitorActionGroupName := terraform.Output(t, ctx.TerratestTerraformOptions(), "action_group_name")
+		resourceGroupName := terraform.OutputContext(t, context.Background(), ctx.TerratestTerraformOptions(), "resource_group_name")
+		MonitorActionGroupName := terraform.OutputContext(t, context.Background(), ctx.TerratestTerraformOptions(), "action_group_name")
 
 		MonitorActionGroup, err := armMonitorClient.Get(context.Background(), resourceGroupName, MonitorActionGroupName, nil)
 		if err != nil {
